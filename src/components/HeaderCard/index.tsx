d0b3899fa20f94634f26dd526680ef08cd685cf5
Bryan <bryanbatista79688@gmail.com>
2022-12-19 11:20:06 -0300

    style: container, header and card first styles

--- src/components/HeaderCard/index.tsx
+++ src/components/HeaderCard/index.tsx
@@ -1,12 +1,14 @@
+import StyledHeaderCard from "./style";
+
 const HeaderCard = () => {
   return (
-    <div>
+    <StyledHeaderCard>
       <img src="" alt="" />
       <p>
         A vida é como um sanduíche, é preciso recheá-la com os
         <span>melhores</span> ingredientes.
       </p>
-    </div>
+    </StyledHeaderCard>
   );
 };
 
